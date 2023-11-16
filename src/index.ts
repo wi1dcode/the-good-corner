c0ce50daaecbaef03ceec49e609a32b5937d2038
WildCode <mago@localhost.local>
2023-11-16 17:15:36 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import adsRouter from "./routes/ads.routes";
 import categoriesRouter from "./routes/categories.routes"
+import datasource from "./lib/datasource";
 
 const app = express();
 app.use(express.json()); //middleware
@@ -8,5 +9,6 @@ app.use("/ads", adsRouter);
 app.use("/categories", categoriesRouter);
 
 app.listen(4000, () => {
+  datasource.initialize()
   console.log("Le serveur est lancé sur le port 4000");
 });
